dc74ab9a581831162749c1e7afc8235b226f96cf
pablozamit <166626152+pablozamit@users.noreply.github.com>
2025-06-23 17:35:54 +0200

    Add default exports to route files

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,1 +1,10 @@
-export { default } from "@/screens/RegisterScreen";
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Register() {
+  return (
+    <View>
+      <Text>register</Text>
+    </View>
+  );
+}
